a6bd6617b206ab44dba1da1215cb8e195c9c01a8
Pawankumar Jawale <pawankumar.jawale@gmail.com>
2022-08-05 10:14:46 +0530

    basic configuration

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
 
+/* import routers */
+import { router as personRouter } from './routers/person-router.js'
+
 /* create an express app and use JSON */
 const app = new express()
 app.use(express.json())
+
+
+/* bring in some routers */
+app.use('/person', personRouter)
+
 
 /* set up swagger in the root */
 const swaggerDocument = YAML.load('api.yaml')
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 /* start the server */
-app.listen(8080)
+app.listen(8080, () => console.log("connected to server"));
